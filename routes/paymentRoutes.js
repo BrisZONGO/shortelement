92d787db119e276fd104adc287ad565c14d60d96
zobridel <efptppaf@gmail.com>
2026-04-19 16:10:00 +0000

    Fix: correction paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,9 +1,19 @@
-const express = require("express");
-const { initPayment, notifyPayment } = require("../controllers/paymentController");
+const express = require('express');
+const {
+  initPaiement,
+  confirmerPaiement,
+  initPayment,
+  notifyPayment
+} = require('../controllers/paymentController');
 
 const router = express.Router();
 
-router.post("/init", initPayment);
-router.post("/notify", notifyPayment);
+// Routes de simulation (pour tests)
+router.post('/init', initPaiement);
+router.post('/confirm', confirmerPaiement);
+
+// Routes CinetPay réelles
+router.post('/cinetpay/init', initPayment);
+router.post('/cinetpay/notify', notifyPayment);
 
 module.exports = router;
